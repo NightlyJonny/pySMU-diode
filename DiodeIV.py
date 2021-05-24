b55eab85e0b663f6f4020790d919eff122f3c06d
NightlyJonny <crissblurum@gmail.com>
2021-05-24 10:41:35 +0200

    Added sweep direction control and minor bugs/cleanup

--- DiodeIV.py
+++ DiodeIV.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 from SerialSMU import SMU
-from time import sleep
 import numpy as np
 
 # Initialize SMU and measure IV
-def acquireIV (port, baudrate, compl, istart, istop, points):
+def acquireIV (port, baudrate, compl, istart, istop, points, direction='BOTH'):
+	# Initialization and main settings
+	NMEAS = 3 # Number of quantities measured (:FORM:ELEM command)
 	smu = SMU(port, baudrate)
-	smu.sendCommands([	
-		':DISP:TEXT:DATA "Initializing...',
-		':DISP:TEXT:STAT ON',			# Set text-mode to show user defined string on display.
+	smu.sendCommands([
 		'*RST',							# Reset instrument to default parameters.
+		':DISP:TEXT:DATA "Initializing..."',
+		':DISP:TEXT:STAT ON',			# Set text-mode to show user defined string on display.
 		':SYST:BEEP:STAT 0',			# Disable the fricking beep
 		
 		':SENS:FUNC "VOLT"',			# Select volt measurement function.
@@ -24,21 +25,38 @@ def acquireIV (port, baudrate, compl, istart, istop, points):
 		f':SOUR:CURR:STOP {istop}',		# Sweep end point.
 		f':SOUR:SWE:POIN {points}',		# Set sweep points.
 		':SOUR:CURR:MODE SWE',			# Set output in sweeping mode.
-		':SOUR:SWE:DIR UP',				# Set sweep direction start->stop.
 		
 		f':TRIG:COUN {points}',			# Set trigger count, MUST BE THE SAME AS SWEEP POINTS.
-		':FORM:ELEM TIME,VOLT,CURR',	# Quantities sent to the PC.
-		# ':OUTP ON',						# Enable output.
-		':DISP:TEXT:DATA "Sweeping...',
-		# ':INIT'							# Start measurements.
+		':FORM:ELEM TIME,VOLT,CURR',	# Quantities sent to the PC. UPDATE "NMEAS" VARIABLE TOO.
+		# ':OUTP ON',					# Enable output.
+		# ':INIT'						# Start measurements.
 	])
 
-	sleep(1)
-	readings = smu.getReadingsArray()
-	print('-= DEBUG: ', readings)
+	# Sweeping up
+	readingsUp = []
+	if (direction == 'UP') or (direction == 'BOTH'):
+		smu.sendCommands([
+			':SOUR:SWE:DIR UP',				# Set sweep direction start->stop.
+			':DISP:TEXT:DATA "Sweeping up..."'
+		])
+		readingsUp = smu.getReadingsArray()
+		readingsUp = readingsUp[:-(len(readingsUp)%NMEAS)] # Discarding extra ungrouped measurements
+
+	# Sweeping down
+	readingsDown = []
+	if (direction == 'DOWN') or (direction == 'BOTH'):
+		smu.sendCommands([
+			':SOUR:SWE:DIR DOWN',			# Set sweep direction stop->start.
+			':DISP:TEXT:DATA "Sweeping down..."'
+		])
+		readingsDown = smu.getReadingsArray()
+		readingsDown = readingsDown[:-(len(readingsDown)%NMEAS)] # Discarding extra ungrouped measurements
+	
+	# Print results and close serial connection
+	print('-= DEBUG: ', readingsUp + readingsDown, '=-')
 	smu.sendCommands([
 		# ':OUTP OFF',
-		':DISP:TEXT:DATA "Done!',
+		':DISP:TEXT:DATA "Done!"',
 		':DISP:TEXT:STAT OFF'
 	])
 	smu.close()
@@ -46,8 +64,7 @@ def acquireIV (port, baudrate, compl, istart, istop, points):
 	# Transform reading from linear array to group of quantities (time, voltage, and current)
 	# e.g., [0.0, 1.0, 1.0, 0.1, 1.5, 3.0] -> [[0.0, 0.1], [1.0, 1.5], [1.0, 3.0]]
 	#         t    V    I    t    V    I   ->      t           V            I
-	NMEAS = 3 # Number of quantities measured (:FORM:ELEM command)
-	readings = np.array(readings[:-(len(readings)%NMEAS)]) # Discarding extra ungrouped measurements
+	readings = np.array(readingsUp + readingsDown)
 	res = readings.reshape((3, -1), order='F') # Reshaping into a matrix with NMEAS row
 
 	return res
